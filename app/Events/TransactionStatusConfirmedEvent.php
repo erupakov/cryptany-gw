0ca8535bc705f990626a93dc29c6ba9f6c601934
Eugene Rupakov <eugene.rupakov@gmail.com>
2017-09-14 22:54:32 +0300

    Added 2 more events

---
+++ app/Events/TransactionStatusConfirmedEvent.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Events;
+
+use App\Transaction;
+use App\Events\Event;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+class TransactionStatusConfirmedEvent extends Event implements ShouldBroadcast
+{
+	/**
+	 * Property to hold event data
+	 *
+	 * @var $transaction
+	 */
+	public $transaction;
+	private $walletHash;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct(Transaction $t)
+    {
+        $this->transaction = $t;
+		$walletHash = $t->wallet->hash;
+    }
+
+    /**
+     * Get the channels the event should be broadcast on.
+     *
+     * @return array
+     */
+    public function broadcastOn()
+    {
+        return ['transactions'.$this->walletHash];
+    }
+}

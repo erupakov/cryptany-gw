854316816293fd69b4585776621c5cc9305049c0
Eugene Rupakov <eugene.rupakov@aurea.com>
2017-09-14 22:44:25 +0300

    Added test txs

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -1,49 +1,84 @@
 <?php
-
+/**
+ * Data generic blockchain actions controller
+ * PHP Version 7
+ *
+ * @category Controller
+ * @package  App\Http\Controllers
+ * @author   Eugene Rupakov <eugene.rupakov@gmail.com>
+ * @license  Apache Common License 2.0
+ * @link     http://cgw.cryptany.io
+ */
 use \App\User;
 use \Illuminate\Http\Request;
+use Carbon\Carbon;
 
 namespace App\Http\Controllers;
-
-class EthController extends Controller
+/**
+ * Data actions controller, used to perform generic actions 
+ *
+ * @category Controller
+ * @package  App\Http\Controllers
+ * @author   Eugene Rupakov <eugene.rupakov@gmail.com>
+ * @license  Apache Common License 2.0
+ * @link     http://cgw.cryptany.io
+ */
+class DataController extends Controller
 {
     private $_token = "f7948af1945f4f779f4deb8988acec91";
     /**
      * Create a new controller instance.
      *
      * @return void
-	 */
+     */
     private $_apiContext;
 
+    /**
+     * Construction for the class, performs API context initialization
+     *
+     * @method __construct
+     * @return nothing
+     */
     public function __construct()
     {
-		$this->_apiContext = new \BlockCypher\Rest\ApiContext(new \BlockCypher\Auth\SimpleTokenCredential($this->_token),'main','eth');
+        $this->_apiContext = new \BlockCypher\Rest\ApiContext(
+            new \BlockCypher\Auth\SimpleTokenCredential($this->_token), 'main', 'eth'
+        );
 
-		$this->_apiContext->setConfig(
-			array(
-			  'mode' => 'sandbox',
-			  'log.LogEnabled' => true,
-			  'log.FileName' => 'BlockCypher.log',
-			  'log.LogLevel' => 'DEBUG'
-			)
-	  	);
+        $this->_apiContext->setConfig(
+            array(
+                'mode' => 'sandbox',
+                'log.LogEnabled' => true,
+                'log.FileName' => 'BlockCypher.log',
+                'log.LogLevel' => 'DEBUG'
+            )
+        );
         $webHook = new \BlockCypher\Api\WebHook(); 
         $webHook->setUrl("http://cgw.cryptany.io/eth/hook/txstat");
         $webHook->setEvent('unconfirmed-tx');
         try {
             $webHook->create($this->_apiContext);
-            //Log::info( "Successfully set unconfirmed-tx hook: " . $webHook );
+            Log::info( "Successfully set unconfirmed-tx hook: " . $webHook );
         }
-		catch (\BlockCypher\Exception\BlockCypherConnectionException $ex) {
-			// This will print the detailed information on the exception. 
-			//REALLY HELPFUL FOR DEBUGGING
-			//Log::error( "Error creating ETH webHook: " . $ex->getData() );
-		}		
+        catch (\BlockCypher\Exception\BlockCypherConnectionException $ex) {
+            // This will print the detailed information on the exception. 
+            //REALLY HELPFUL FOR DEBUGGING
+            Log::error("Error creating ETH webHook: " . $ex->getData());
+        }
     }
 
-    //
-	public function getTransientAddress($id)
-	{
+    /**
+     * Method for handling creation of new transient wallet API method
+     *
+     * @param \Illuminate\Http\Request $request Request to process
+     * @param string                   $id      appToken of the caller
+     *
+     * @method getTransientAddress
+     *
+     * @return nothing
+     */
+    public function getTransientAddress(Request $request, $id)
+    {
 //		$user = App\User::where( 'appToken', $id );
 
 //		if (isset($user)) {
@@ -52,25 +87,25 @@ class EthController extends Controller
 
 //		$wallet = new App\Wallet;
 //		$wallet->userId = $user->id;
-		$addressClient = new \BlockCypher\Client\AddressClient($this->_apiContext);
-		$address = $addressClient->generateAddress();
-	
-		//Log::info('New address generated:' . $address->getAddress());
+        $addressClient = new \BlockCypher\Client\AddressClient($this->_apiContext);
+        $address = $addressClient->generateAddress();
 
-		return json_encode( ['address'=> $address->getAddress()] );
-	}
+        //Log::info('New address generated:' . $address->getAddress());
 
-	// Returns status of the transaction given its hash
-	public function getTxStatus($hash)
-	{
-		$txClient = new \BlockCypher\Client\TXClient( $this->_apiContext );
-		$tx = $txClient->get($hash);
-		return json_encode( ['confirmed'=>$tx->confirmed] );
-	}
+        return json_encode(['address'=> $address->getAddress()]);
+    }
 
-	// Hook to catch blockchain events
-	public function getTxStatusHook($request)
-	{
-		Log::info('Got hook request:'. $request);
-	}
+    // Returns status of the transaction given its hash
+    public function getTxStatus($hash)
+    {
+        $txClient = new \BlockCypher\Client\TXClient($this->_apiContext);
+        $tx = $txClient->get($hash);
+        return json_encode(['confirmed'=>$tx->confirmed]);
+    }
+
+    // Hook to catch blockchain events
+    public function getTxStatusHook($request)
+    {
+        Log::info('Got hook request:'. $request);
+    }
 }
--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,5 +1,14 @@
 <?php
-
+/**
+ * Model factory for cryptany service
+ * PHP Version 7
+ *
+ * @category DB
+ * @package  Database\ModelFactory
+ * @author   Eugene Rupakov <eugene.rupakov@gmail.com>
+ * @license  Apache Common License 2.0
+ * @link     http://cgw.cryptany.io
+ */
 /*
 |--------------------------------------------------------------------------
 | Model Factories
@@ -14,18 +23,18 @@
 $factory->define(App\User::class, function (Faker\Generator $faker) {
     return [
         'first_name' => $faker->firstName,
-		'family_name' => $faker->lastName,
+        'family_name' => $faker->lastName,
         'email' => $faker->unique()->safeEmail,
-		'password' => $faker->password,
-		'pin' => '1234'
+        'password' => $faker->password,
+        'pin' => '1234'
     ];
 });
 
 $factory->define(App\APIUser::class, function (Faker\Generator $faker) {
     return [
-		'username' => 'android',
+        'username' => 'android',
         'appToken' => str_random(10),
-		'expiryDate' => '2018-10-01',
-		'description' => 'test apiuser'
+        'expiryDate' => '2018-10-01',
+        'description' => 'test apiuser'
     ];
 });

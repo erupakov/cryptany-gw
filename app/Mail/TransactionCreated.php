6f7af66dd7f3ad2c27d86d975900e895516485c1
Eugene Rupakov <eugene.rupakov@aurea.com>
2017-09-22 21:56:39 +0300

    Mail management installed

---
+++ app/Mail/TransactionCreated.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Ethereum blockchain actions controller
+ * PHP Version 7
+ *
+ * @category Controller
+ * @package  App\Http\Controllers
+ * @author   Eugene Rupakov <eugene.rupakov@gmail.com>
+ * @license  Apache Common License 2.0
+ * @link     http://cgw.cryptany.io
+ */
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+/**
+ * Mail template about success creating transaction
+ *
+ * @category Mail
+ * @package  App\Mail
+ * @author   Eugene Rupakov <eugene.rupakov@gmail.com>
+ * @license  Apache Common License 2.0
+ * @link     http://cgw.cryptany.io
+ */
+class TransactionCreated extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('emails.tx_created');
+    }
+}

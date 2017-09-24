ffe532f000077a90740cd355552286f8a68282ab
Eugene Rupakov <eugene.rupakov@gmail.com>
2017-09-24 20:04:01 +0300

    Fixed emails

--- resources/views/emails/tx_confirmed.blade.php
+++ resources/views/emails/tx_confirmed.blade.php
@@ -2,26 +2,26 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title>Cryptany: Transaction confirmed</title>
+<title>Cryptany: Transfer confirmed</title>
 </head>
 <body style="margin: 10px 0 0 0; padding: 0; background-color: #f4f4f4;">
 
 <div style="text-align: center; margin-bottom: 20px;">
-	<span style="font-family: Sans-serif; font-size: 12px;">Cannot view this email? Open your transaction <a href="https://mobile.cryptany.io/transit/{{ $txId }}">here</a> in your favorite browser</span>
+	<span style="font-family: Sans-serif; font-size: 12px;">Cannot view this email? Open your transfer <a href="https://mobile.cryptany.io/transit/{{ $txId }}">here</a> in your favorite browser</span>
 </div>
 <div style="margin: auto; width: 920px; background-color: #fff;"><!-- Main container -->
-<h3>Your transaction was confirmed by the blockchain</h3>
+<h3>Your transfer was confirmed by the blockchain</h3>
 <p>Hi! This is <strong>Cryptany</strong> notification system. We want to inform you that your 
-Ethereum to USD funds transfer transaction was confirmed by blockchain and is we currently processing it. You will get your funds on your bank account soon.</p>
-<p>Please find more details about transaction below:</p>
+Ethereum to USD funds transfer was confirmed by blockchain and is we currently processing it. You will get your funds on your bank account soon.</p>
+<p>Please find more details about transfer below:</p>
 <ul>
-<li>Transaction ID: {{ $txId }}</li>
+<li>Transfer ID: {{ $txId }}</li>
 <li>Wallet address: {{ $address }}</li>
-<li>Transaction sum: {{ $srcAmount }} ETH => {{ $dstAmount }} USD</li>
-<li>Transaction date: {{ $txDate }}</li>
+<li>Transfer sum: {{ $srcAmount }} ETH => {{ $dstAmount }} USD</li>
+<li>Transfer date: {{ $txDate }}</li>
 <li>QR code: <img src="https://chart.googleapis.com/chart?chs=150x150&cht=qr&chl={{ $address }}&choe=UTF-8" alt="address qr code"></li>
 </ul>
-<p>You can track your transaction status by using <a href="https://mobile.cryptany.io/transit/{{ $txId }}">this link</a> via Cryptany.io mobile application</p>
+<p>You can track your transfer status by using <a href="https://mobile.cryptany.io/transit/{{ $txId }}">this link</a> via Cryptany.io mobile application</p>
 </div> <!-- Main container -->
 
 </body>
--- resources/views/emails/tx_created.blade.php
+++ resources/views/emails/tx_created.blade.php
@@ -2,26 +2,26 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title>Cryptany: Transaction created</title>
+<title>Cryptany: Transfer created</title>
 </head>
 <body style="margin: 10px 0 0 0; padding: 0; background-color: #f4f4f4;">
 
 <div style="text-align: center; margin-bottom: 20px;">
-	<span style="font-family: Sans-serif; font-size: 12px;">Cannot view this email? Open your transaction <a href="https://mobile.cryptany.io/transit/{{ $txId }}">here</a> in your favorite browser</span>
+	<span style="font-family: Sans-serif; font-size: 12px;">Cannot view this email? Open your transfer <a href="https://mobile.cryptany.io/transit/{{ $txId }}">here</a> in your favorite browser</span>
 </div>
 <div style="margin: auto; width: 920px; background-color: #fff;"><!-- Main container -->
-<h3>Your transaction was successfully created</h3>
+<h3>Your transfer was successfully created</h3>
 <p>Hi! This is <strong>Cryptany</strong> notification system. We want to inform you that your 
-Ethereum to USD funds transfer transaction was successfully created and is currently waiting for your payment and confirmation.</p>
+Ethereum to USD funds transfer was successfully created and is currently waiting for your payment and confirmation.</p>
 <p>Please find more details about transaction below:</p>
 <ul>
-<li>Transaction ID: {{ $txId }}</li>
+<li>Transfer ID: {{ $txId }}</li>
 <li>Wallet address: {{ $address }}</li>
-<li>Transaction sum: {{ $srcAmount }} ETH => {{ $dstAmount }} USD</li>
-<li>Transaction date: {{ $txDate }}</li>
+<li>Transfer sum: {{ $srcAmount }} ETH => {{ $dstAmount }} USD</li>
+<li>Transfer date: {{ $txDate }}</li>
 <li>QR code: <img src="https://chart.googleapis.com/chart?chs=150x150&cht=qr&chl={{ $address }}&choe=UTF-8" alt="address qr code"></li>
 </ul>
-<p>You can track your transaction status by using <a href="https://mobile.cryptany.io/transit/{{ $txId }}">this link</a> via Cryptany.io mobile application</p>
+<p>You can track your transfer status by using <a href="https://mobile.cryptany.io/transit/{{ $txId }}">this link</a> via Cryptany.io mobile application</p>
 </div> <!-- Main container -->
 
 </body>
